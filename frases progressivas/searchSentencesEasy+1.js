62a2cb5bb820989d798e0c230a9fd2953ad44325
Caio <kayoroxa@gmail.com>
2023-01-20 04:10:58 -0300

    fix bug search

--- frases progressivas/searchSentencesEasy+1.js
+++ frases progressivas/searchSentencesEasy+1.js
@@ -5,22 +5,36 @@ const txt = fs.readFileSync(
   joinPath(__dirname, '../890k sentences in english.txt'),
   { encoding: 'utf-8' }
 )
-let allWords = require('../words most used.json').slice(0, 40)
+let commonWords = require('../words most used.json').slice(0, 40)
 
 const allSentences = txt.split('\r\n')
 
+function checkWords(sentence, additionalWords = '') {
+  // sentence = sentence.replace(/[^\w\s]/gi, '')
+  const re = new RegExp(`\\b${additionalWords}\\b`, 'i')
+  if (!re.test(sentence)) return false
+
+  const newSentence = sentence.replace(re, '').replace(/\s{2,}/g, ' ')
+  const words = newSentence?.match(/[a-zA-Z][’'a-zA-Z]*/gi)
+
+  if (!words) return false
+
+  for (const word of words) {
+    if (!commonWords.find(c => c === word.toLowerCase())) {
+      return false
+    }
+  }
+  return true
+}
+
 function getAll(quero) {
-  if (quero) allWords.push(quero)
+  // if (quero) commonWords = [quero, ...commonWords]
 
   const result = allSentences.filter(sentence => {
     const words = sentence.toLowerCase().match(/[a-zA-Z][’'a-zA-Z]*/gi)
     if (!words) return false
-    const isWool = words.every(w => allWords.includes(w.toLowerCase()))
 
-    if (!quero) return isWool
-    const is = words.includes(quero)
-
-    return isWool && is
+    return checkWords(sentence, quero)
   })
 
   // console.log(result.join('\n'))
@@ -29,50 +43,22 @@ function getAll(quero) {
 }
 
 const query = `
-happened
-called
-corrected
-wanted
-saved
-started
-scared
-used
-tried
-worked
-supposed
-asked
-ruined
-changed
-missed
-played
-needed
-loved
-fired
-tired
-named
-liked
-stopped
-learned
-looked
-finished
-worried
-talked
-hundred
-interested
-moved
-closed
+let me
 `
 
 // const s = getAll().filter(f => f.split(' ').length > 5)
 // console.log(s.join('\n'))
 
-// const allAll = query
-//   .split('\n')
-//   .filter(Boolean)
-//   .map((q, i) => {
-//     console.clear()
-//     console.log(i, query.split('\n').filter(Boolean).length)
-//     return getAll(q)
-//   })
+const allAll = query
+  .split('\n')
+  .filter(Boolean)
+  .map((q, i) => {
+    console.clear()
+    console.log(i, query.split('\n').filter(Boolean).length)
+    return getAll(q)
+  })
 
-// fs.writeFileSync(__dirname + '/result-ed.json', JSON.stringify(allAll, null, 2))
+fs.writeFileSync(
+  __dirname + '/result-anchor.json',
+  JSON.stringify(allAll, null, 2)
+)
